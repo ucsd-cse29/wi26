0566ed4ab0c5f6935efad50963c5418102b7fb05
Joe Politz <joe.politz@gmail.com>
2026-01-06 09:26:39 -0800

    lecture files

---
+++ src/lec/01-06-welcome/bits.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  char capital_a = 'A';
+  char sixty_five = 65;
+
+  printf("capital_a:  %c %d\n", capital_a, capital_a);
+  printf("sixty_five: %c %d\n", sixty_five, sixty_five);
+}
