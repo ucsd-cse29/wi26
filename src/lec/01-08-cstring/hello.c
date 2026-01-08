cc3045eec9be703bd6565f229e2b3745b44cab34
Joe Politz <joe.politz@gmail.com>
2026-01-08 09:58:19 -0800

    lecture 2

---
+++ src/lec/01-08-cstring/hello.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void uppercase(char str[]) {
+
+
+
+
+
+
+
+
+
+
+
+}
+
+void inspect(char str[]) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        char c = str[i];
+        printf("(%c %d) ", c, c);
+    }
+    printf("\n");
+}
+
+int main() {
+    char message[] = "Hello!";
+    printf("%s\n", message);
+    printf("(%c %d) (%c %d) (%c %d) (%c %d) (%c %d) (%c %d) (%c %d)\n",
+           message[0], message[0],
+           message[1], message[1],
+           message[2], message[2],
+           message[3], message[3],
+           message[4], message[4],
+           message[5], message[5],
+           message[6], message[6]);
+    inspect(message);
+
+    // After this call, message should contain "HELLO!"
+    uppercase(message);
+    printf("After uppercase: %s\n", message);
+}
